f28b528592e1c1ab5c978e2d71c9559ae26bdec9
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-04 22:18:26 +0100

    Add safely_get_value function to safely retrieve a value from a mapping

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""The Mapping
+"""
+from typing import Mapping, Any, Union, TypeVar
+
+
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[TypeVar, None] = None
+) -> Union[Any, TypeVar]:
+    """None
+
+    Args:
+        dct (Mapping): None
+        key (Any): _description_
+        default (Union[TypeVar, None], optional): Returns a TypeVar.
+        Defaults to None.
+
+    Returns:
+        Union[Any, TypeVar]:Returns A value
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
