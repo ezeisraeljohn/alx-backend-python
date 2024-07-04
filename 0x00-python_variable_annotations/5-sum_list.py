daf742e3c2ea672e67b05a3707c7bb820faa9801
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-04 17:42:54 +0100

    chore: Add sum_list function to calculate the sum of a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/bin/bash/env python3
+
+"""Adds the following changes
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of the lists as flaot
+
+    Args:
+        input_list (List[float]): list of floats
+
+    Returns:
+        float: Returns the sum of the list as float
+    """
+    return sum(input_list)
