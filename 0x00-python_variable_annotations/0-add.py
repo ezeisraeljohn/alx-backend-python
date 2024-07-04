4b15c7c480ec1dd13211d66c87fc5b4f4fe94842
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-04 16:15:03 +0100

    Fix import statement in 0-main.js

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" This Method houses the function that sums a float"""
+
+
+def add(a: float, b: float) -> float:
+    """This function takes two float and return your sum
+
+    Args:
+        a (float): The first float
+        b (float): The second float
+
+    Returns:
+        float: The sum of the float
+    """
+    return a + b
+
