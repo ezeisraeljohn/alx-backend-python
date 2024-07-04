068de900fa73ed48a2ff47619360467dcd32c776
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-04 16:42:06 +0100

    chore: Add concat function in 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""This concats a string a returns a string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function concats a string
+
+    Args:
+        str1 (str): a string
+        str2 (str): a string
+
+    Returns:
+        str: returns the concatenated string
+    """
+    return "{}{}".format(str1, str2)
