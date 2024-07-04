f0d7bc6510ef442aded8fe2cf332fb602105466c
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-04 17:06:34 +0100

    Add floor function to round a float down

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import math
+
+"""This is returns a rounded float
+"""
+
+
+def floor(n: float) -> int:
+    """Returns a rounded float
+
+    Args:
+        n (float): the float to floor
+
+    Returns:
+        float: the floored float to return
+    """
+    return math.floor(n)
